7a6be16f338047294bc58a98d7a39f70f2be19cd
idkshite <85193567+idkshite@users.noreply.github.com>
2022-11-10 21:39:50 +0000

    add todayConsumeMinutes

--- records/todaysConsumeMinutes.ts
+++ records/todaysConsumeMinutes.ts
@@ -1,7 +1,12 @@
+import { getMongoConnection } from "../mongodb";
+import { DateTime } from "luxon";
 
-
-export async function getTodaysConsumeMinutes(){
+export async function getTodaysConsumeMinutes() {
   const mongo = await getMongoConnection();
 
+  const today = DateTime.now.toISO();
   
+  mongo.collection("records").find()
+
+
 }
